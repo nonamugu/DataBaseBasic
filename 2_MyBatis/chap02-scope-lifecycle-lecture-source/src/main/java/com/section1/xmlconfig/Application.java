68a198bdc38cbb7d4b0d7846db7632df309baa02
nonamugu <0908jhk@naver.com>
2023-05-25 14:09:31 +0900

    20230525 chap02 commit

---
+++ 2_MyBatis/chap02-scope-lifecycle-lecture-source/src/main/java/com/section1/xmlconfig/Application.java
@@ -0,0 +1,15 @@
+package com.section1.xmlconfig;
+
+import static com.section1.xmlconfig.Template.getSqlSession;
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println(getSqlSession());
+        System.out.println(getSqlSession());
+        System.out.println(getSqlSession());
+        System.out.println(getSqlSession());
+        System.out.println(getSqlSession());
+
+
+    }
+}
---
+++ 2_MyBatis/chap02-scope-lifecycle-lecture-source/src/main/java/com/section1/xmlconfig/Template.java
@@ -0,0 +1,32 @@
+package com.section1.xmlconfig;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Template {
+    private static SqlSessionFactory sqlSessionFactory;
+
+    public static SqlSession getSqlSession() {
+
+        if(sqlSessionFactory == null) {
+            String resource = "mybatis-config.xml";
+            try {
+                InputStream inputStream = Resources.getResourceAsStream(resource);
+                sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        SqlSession sqlSession = sqlSessionFactory.openSession(false);
+
+        System.out.println("sqlSessionFactory의 hashCode() : " + sqlSessionFactory.hashCode());
+        System.out.println("sqlSession의 hashCode() : " + sqlSession);
+
+        return sqlSession;
+    }
+}
