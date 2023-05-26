e15339254e8c0b049c51d8ce3b4a42da3abf1542
nonamugu <0908jhk@naver.com>
2023-05-26 16:11:17 +0900

    20230526 section3 commit

---
+++ 2_MyBatis/chap03-crud-lecture-source/src/main/java/com/section3/remix/MenuController.java
@@ -0,0 +1,95 @@
+package com.section3.remix;
+
+import com.section3.remix.MenuDTO;
+import com.section3.remix.MenuService;
+import com.section3.remix.PrintResult;
+
+import java.util.List;
+import java.util.Map;
+
+public class MenuController {
+
+    private final PrintResult printResult;
+    private final MenuService menuService;
+
+    public MenuController() {
+        printResult = new PrintResult();
+        menuService = new MenuService();
+    }
+    // 1. 전체 메뉴 조회하기
+    public void selectAllMenu() {
+
+        List<MenuDTO> menuList = menuService.selectAllmenu();
+
+        if(menuList != null) {
+            printResult.printMenuList(menuList);
+        } else {
+            printResult.printErrorMessage("selectList");
+        }
+    }
+
+    // 2. 메뉴 코드로 메뉴 조회하기
+    public void selectMenuByCode(Map<String, String> parameter) {
+        int code = Integer.parseInt(parameter.get("code"));
+        MenuDTO menu = menuService.selectMenuByCode(code);
+
+        if(menu != null) {
+            printResult.printMenu(menu);
+        } else {
+            printResult.printErrorMessage("selectOne");
+        }
+
+    }
+
+    // 3. 메뉴 등록하기
+    public void registMenu(Map<String, String> parameter) {
+        String name = parameter.get("name");
+        int price = Integer.parseInt(parameter.get("price"));
+        int categoryCode = Integer.parseInt(parameter.get("categoryCode"));
+
+        MenuDTO menu = new MenuDTO();
+        menu.setName(name);
+        menu.setPrice(price);
+        menu.setCategoryCode(categoryCode);
+
+        if(menuService.registMenu(menu)) {
+            printResult.printSuccessMessage("insert");
+        } else {
+            printResult.printErrorMessage("insert");
+        }
+    }
+
+    // 4. 메뉴 수정하기
+    public void modifyMenu(Map<String, String> parameter) {
+
+        int code = Integer.parseInt(parameter.get("code"));
+        String name = parameter.get("name");
+        int price = Integer.parseInt(parameter.get("price"));
+        int categoryCode = Integer.parseInt(parameter.get("categoryCode"));
+
+        MenuDTO menu = new MenuDTO();
+        menu.setCode(code);
+        menu.setName(name);
+        menu.setPrice(price);
+        menu.setCategoryCode(categoryCode);
+
+        if(menuService.modifyMenu(menu)) {
+            printResult.printSuccessMessage("update");
+        } else {
+            printResult.printErrorMessage("update");
+        }
+
+    }
+
+    // 5. 메뉴 삭제하기
+    public void deleteMenu(Map<String, String> parameter) {
+        int code = Integer.parseInt(parameter.get("code"));
+
+        if(menuService.deleteMenu(code)) {
+            printResult.printSuccessMessage("delete");
+        } else {
+            printResult.printErrorMessage("delete");
+        }
+    }
+
+}
---
+++ 2_MyBatis/chap03-crud-lecture-source/src/main/java/com/section3/remix/MenuMapper.java
@@ -0,0 +1,16 @@
+package com.section3.remix;
+
+import java.util.List;
+
+public interface MenuMapper {
+
+    List<MenuDTO> selectAllMenu();
+
+    MenuDTO selectMenuByCode(int code);
+
+    int insertMenu(MenuDTO menu);
+
+    int updateMenu(MenuDTO menu);
+
+    int deleteMenu(int code);
+}
